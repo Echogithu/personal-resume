099a4f8d114488391b21f56c3343ca7d69678d5d
蔡泽萍 <1391531024@qq.com>
2024-02-24 17:32:23 +0800

    feat: 添加简历下载

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,16 +1,27 @@
 import { Link } from "react-router-dom";
 
 const CTA = () => {
+  function downloadFile() {
+    fetch("/assets/resume.pdf", {
+      responseType: "blob",
+    })
+      .then((response) => response.blob())
+      .then((blob) => {
+        var url = window.URL.createObjectURL(blob);
+        var a = document.createElement("a");
+        a.href = url;
+        a.download = "前端开发工程师_蔡泽萍_13433680844.pdf";
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+      });
+  }
+
   return (
     <section className="cta">
       <p className="cta-text">如果您对我感兴趣 ➡️</p>
-      <div
-        onClick={window.open(
-          `https://github.com/Echogithu/chair-configurator/blob/main/public/demo.png?raw=true`
-        )}
-        className="btn cursor-pointer"
-      >
-        查看简历
+      <div onClick={downloadFile} className="btn cursor-pointer">
+        下载简历
       </div>
     </section>
   );
--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -5,9 +5,9 @@ import { arrow } from "../assets/icons";
 const HomeInfo = ({ currentStage }) => {
   if (currentStage === 1)
     return (
-      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+      <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
         Hi, I'm
-        <span className='font-semibold mx-2 text-white'>Echo</span>
+        <span className="font-semibold mx-2 text-white">Echo</span>
         👋
         <br />
         欢迎来到我的小岛 ⛳️
@@ -16,20 +16,20 @@ const HomeInfo = ({ currentStage }) => {
 
   if (currentStage === 2) {
     return (
-      <div className='info-box'>
-        <p className='font-medium sm:text-xl text-center'>
-          <span className="mr-4">📍 深圳</span> 
-          <span className="mr-4">💼 3年</span> 
-          <span>🎓 本科</span> 
-          <br /> 
+      <div className="info-box">
+        <p className="font-medium sm:text-xl text-center">
+          <span className="mr-4">📍 深圳</span>
+          <span className="mr-4">💼 3年</span>
+          <span>🎓 本科</span>
+          <br />
           <span>📲 13433680844</span>
-          <br /> 
+          <br />
           <span>💌 13433680844@163.com</span>
         </p>
 
-        <Link to='/about' className='neo-brutalism-white neo-btn'>
+        <Link to="/about" className="neo-brutalism-white neo-btn">
           了解更多
-          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+          <img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
         </Link>
       </div>
     );
@@ -37,14 +37,14 @@ const HomeInfo = ({ currentStage }) => {
 
   if (currentStage === 3) {
     return (
-      <div className='info-box'>
-        <p className='font-medium text-center sm:text-xl'>
+      <div className="info-box">
+        <p className="font-medium text-center sm:text-xl">
           如果好奇我的经历和项目 👀
         </p>
 
-        <Link to='/projects' className='neo-brutalism-white neo-btn'>
+        <Link to="/projects" className="neo-brutalism-white neo-btn">
           了解更多
-          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+          <img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
         </Link>
       </div>
     );
@@ -52,16 +52,16 @@ const HomeInfo = ({ currentStage }) => {
 
   if (currentStage === 4) {
     return (
-      <div className='info-box'>
-      <p className='font-medium sm:text-xl text-center'>
-       如果您需要招聘一名前端开发 👩🏻‍💻
-      </p>
+      <div className="info-box">
+        <p className="font-medium sm:text-xl text-center">
+          如果您需要招聘一名前端开发 👩🏻‍💻
+        </p>
 
-      <Link to='/contact' className='neo-brutalism-white neo-btn'>
-        欢迎联系我
-        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
-      </Link>
-    </div>
+        <Link to="/" className="neo-brutalism-white neo-btn">
+          欢迎联系我
+          {/*<img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />*/}
+        </Link>
+      </div>
     );
   }
 
